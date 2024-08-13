3fb8e0860f28f21b33a773c4c2a4bba84e5fde07
Apurv Ajay Kumar <aakapurv@gmail.com>
2024-08-13 21:14:49 +0530

    feat: Simplify MainActivity and SoundProfileRecyclerAdapter
    - Extracted repeated code in `MainActivity` into helper methods (`setupWindowInsets`, `setupRecyclerView`, `observeViewModel`, `showLoading`, `showEmptyState`, `showSuccessState`, `setupSelectionTracker`, `updateToolbarMenu`, `handleMenuItemClick`, `showError`).
    - Simplified `getCurrentVolume` in `MainActivity` by using a helper function `getVolume` to reduce redundancy.
    - Removed redundant `apply` blocks in `MainActivity` where not necessary.
    - Simplified `onCreate` in `MainActivity` by breaking it into smaller methods for better readability.
    - Added `selectAll` and `getSelectedSoundProfile` methods in `SoundProfileRecyclerAdapter` to handle selection logic.
    - Implemented `bind`, `bindSelection`, and `getItemDetails` methods in `CardSoundProfileItemHolder` for better encapsulation and readability.
    - Added `SpaceBetweenItemDecorator` to handle item spacing in `RecyclerView`.
    - Added `SoundProfileItemKeyProvider` and `SoundProfileItemDetailsLookup` for selection tracking in `RecyclerView`.

--- app/src/main/java/com/a3/soundprofiles/MainActivity.kt
+++ app/src/main/java/com/a3/soundprofiles/MainActivity.kt
@@ -23,12 +23,7 @@ import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.a3.soundprofiles.AboutDialog.Companion.shareApp
 import com.a3.soundprofiles.core.data.SoundProfile
-import com.a3.soundprofiles.core.main.MainState
-import com.a3.soundprofiles.core.main.MainViewModel
-import com.a3.soundprofiles.core.main.SoundProfileItemDetailsLookup
-import com.a3.soundprofiles.core.main.SoundProfileItemKeyProvider
-import com.a3.soundprofiles.core.main.SoundProfileRecyclerAdapter
-import com.a3.soundprofiles.core.main.SpaceBetweenItemDecorator
+import com.a3.soundprofiles.core.main.*
 import com.a3.soundprofiles.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Date
@@ -36,224 +31,214 @@ import java.util.Date
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-  val mainViewModel: MainViewModel by viewModels()
-  private lateinit var tracker: SelectionTracker<Long>
-  private lateinit var _binding: ActivityMainBinding
-  private val binding
-    get() = _binding
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var tracker: SelectionTracker<Long>
+    private lateinit var binding: ActivityMainBinding
 
-  private var soundProfileManagerLauncher: ActivityResultLauncher<Intent> =
-      registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        Log.d("MainActivity", "Result code: ${result.resultCode}")
-        if (result.resultCode == Activity.RESULT_OK) {
-          mainViewModel.loadAllSoundProfiles()
+    private val soundProfileManagerLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                mainViewModel.loadAllSoundProfiles()
+            }
         }
-      }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    _binding = ActivityMainBinding.inflate(layoutInflater)
-    enableEdgeToEdge()
-    setContentView(binding.root)
-    ViewCompat.setOnApplyWindowInsetsListener(binding.appBarLayout) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(0, systemBars.top, 0, 0)
-      insets
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        setupWindowInsets()
+        setSupportActionBar(binding.toolbar)
+        mainViewModel.loadAllSoundProfiles()
+        setupRecyclerView()
+        observeViewModel()
+        binding.fab.setOnClickListener {
+            val intent = SoundProfileManager.createIntent(this)
+            soundProfileManagerLauncher.launch(intent)
+        }
     }
-    ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(0, 0, 0, systemBars.bottom)
-      insets
+
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.appBarLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBars.top, 0, 0)
+            insets
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, 0, 0, systemBars.bottom)
+            insets
+        }
     }
-    setSupportActionBar(binding.toolbar)
 
-    mainViewModel.loadAllSoundProfiles()
-
-    val soundProfileRecyclerAdapter =
-        SoundProfileRecyclerAdapter(
-            emptyList<SoundProfile>().toMutableList(),
+    private fun setupRecyclerView() {
+        val adapter = SoundProfileRecyclerAdapter(
+            mutableListOf(),
             soundProfileManagerLauncher,
-            mainViewModel::toggleIsActive)
-    val linearLayoutManager = LinearLayoutManager(this)
-    binding.recyclerView.apply {
-      adapter = soundProfileRecyclerAdapter
-      layoutManager = linearLayoutManager
+            mainViewModel::toggleIsActive
+        )
+        binding.recyclerView.apply {
+            this.adapter = adapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(SpaceBetweenItemDecorator(4))
+        }
     }
 
-    mainViewModel.state.observe(this) { state ->
-      when (state) {
-        is MainState.Loading -> {
-          binding.nestedScrollView.visibility = View.GONE
-          binding.emptyProfileIndicator.visibility = View.GONE
-          binding.loadingIndicator.visibility = View.VISIBLE
+    private fun observeViewModel() {
+        mainViewModel.state.observe(this) { state ->
+            when (state) {
+                is MainState.Loading -> showLoading()
+                is MainState.Empty -> showEmptyState()
+                is MainState.Success -> showSuccessState(state.soundProfiles)
+                is MainState.Error -> showError(state.message)
+            }
         }
-        is MainState.Empty -> {
-          soundProfileRecyclerAdapter.updateSoundProfiles(emptyList())
-          binding.nestedScrollView.visibility = View.GONE
-          binding.loadingIndicator.visibility = View.GONE
-          binding.emptyProfileIndicator.visibility = View.VISIBLE
-        }
-        is MainState.Success -> {
-
-          binding.loadingIndicator.visibility = View.GONE
-          binding.emptyProfileIndicator.visibility = View.GONE
-          binding.nestedScrollView.visibility = View.VISIBLE
-
-          val soundProfileRecyclerAdapter =
-              binding.recyclerView.adapter as SoundProfileRecyclerAdapter
-          soundProfileRecyclerAdapter.updateSoundProfiles(state.soundProfiles)
-
-          tracker =
-              SelectionTracker.Builder(
-                      "soundProfileSelection",
-                      binding.recyclerView,
-                      SoundProfileItemKeyProvider(binding.recyclerView),
-                      SoundProfileItemDetailsLookup(binding.recyclerView),
-                      StorageStrategy.createLongStorage())
-                  .withSelectionPredicate(SelectionPredicates.createSelectAnything())
-                  .build()
-          soundProfileRecyclerAdapter.tracker = tracker
-
-          tracker.addObserver(
-              object : SelectionTracker.SelectionObserver<Long>() {
-                override fun onSelectionChanged() {
-                  super.onSelectionChanged()
-                  binding.toolbar.apply {
-                    menu.clear()
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-                    if (tracker.selection.size() > 0) {
-                      title = getString(R.string.selected, tracker.selection.size())
-                      inflateMenu(R.menu.profiles_selected_menu)
-                      menu.findItem(R.id.action_edit).isVisible = tracker.selection.size() == 1
-                      supportActionBar?.setHomeAsUpIndicator(R.drawable.close_24)
-                      supportActionBar?.setDisplayHomeAsUpEnabled(true)
-                      setOnMenuItemClickListener { menuItem ->
-                        when (menuItem.itemId) {
-                          R.id.action_edit -> {
-                            val intent =
-                                SoundProfileManager.createIntent(
-                                    this@MainActivity, tracker.selection.first().toInt())
-                            soundProfileManagerLauncher.launch(intent)
-                            tracker.clearSelection()
-                            true
-                          }
-                          R.id.action_delete -> {
-                            val selectedProfiles =
-                                soundProfileRecyclerAdapter.getSelectedSoundProfile()
-                            mainViewModel.deleteSoundProfiles(selectedProfiles)
-                            tracker.clearSelection()
-                            true
-                          }
-
-                          R.id.action_delete_all -> {
-                            mainViewModel.deleteAllSoundProfiles()
-                            tracker.clearSelection()
-                            true
-                          }
-
-                          R.id.action_select_all -> {
-                            soundProfileRecyclerAdapter.selectAll()
-                            true
-                          }
-
-                          else -> false
-                        }
-                      }
-                    } else {
-                      title = getString(R.string.app_name)
-                      inflateMenu(R.menu.main_menu)
-                      setOnMenuItemClickListener { menuItem -> onOptionsItemSelected(menuItem) }
-                    }
-                  }
-                }
-              })
-        }
-
-        is MainState.Error -> {
-          // Handle error
-          Toast.makeText(this, state.message, Toast.LENGTH_SHORT).show()
-        }
-      }
     }
 
-    // Add ItemDecoration to the RecyclerView
-    // only once to prevent extra space on every update.
-    binding.recyclerView.addItemDecoration(SpaceBetweenItemDecorator(4))
-    binding.fab.setOnClickListener {
-      val intent = SoundProfileManager.createIntent(this)
-      soundProfileManagerLauncher.launch(intent)
+    private fun showLoading() {
+        binding.apply {
+            nestedScrollView.visibility = View.GONE
+            emptyProfileIndicator.visibility = View.GONE
+            loadingIndicator.visibility = View.VISIBLE
+        }
     }
-  }
 
-  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-    menuInflater.inflate(R.menu.main_menu, menu)
-    return true
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    return when (item.itemId) {
-      android.R.id.home -> {
-        tracker.clearSelection()
-        true
-      }
-      R.id.infoMenu -> showInfoMenu()
-      R.id.shareMenu -> {
-        shareApp(this)
-        true
-      }
-      else -> super.onOptionsItemSelected(item)
+    private fun showEmptyState() {
+        (binding.recyclerView.adapter as SoundProfileRecyclerAdapter).updateSoundProfiles(emptyList())
+        binding.apply {
+            nestedScrollView.visibility = View.GONE
+            loadingIndicator.visibility = View.GONE
+            emptyProfileIndicator.visibility = View.VISIBLE
+        }
     }
-  }
 
-  private fun showInfoMenu(): Boolean {
-    val aboutDialogBox = AboutDialog()
-    aboutDialogBox.show(supportFragmentManager, "about_dialog_box")
-    return true
-  }
+    private fun showSuccessState(soundProfiles: List<SoundProfile>) {
+        binding.apply {
+            loadingIndicator.visibility = View.GONE
+            emptyProfileIndicator.visibility = View.GONE
+            nestedScrollView.visibility = View.VISIBLE
+        }
+        val adapter = binding.recyclerView.adapter as SoundProfileRecyclerAdapter
+        adapter.updateSoundProfiles(soundProfiles)
+        setupSelectionTracker(adapter)
+    }
+
+    private fun setupSelectionTracker(adapter: SoundProfileRecyclerAdapter) {
+        tracker = SelectionTracker.Builder(
+            "soundProfileSelection",
+            binding.recyclerView,
+            SoundProfileItemKeyProvider(binding.recyclerView),
+            SoundProfileItemDetailsLookup(binding.recyclerView),
+            StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(SelectionPredicates.createSelectAnything()).build()
+        adapter.tracker = tracker
+        tracker.addObserver(object : SelectionTracker.SelectionObserver<Long>() {
+            override fun onSelectionChanged() {
+                super.onSelectionChanged()
+                updateToolbarMenu()
+            }
+        })
+    }
+
+    private fun updateToolbarMenu() {
+        binding.toolbar.apply {
+            menu.clear()
+            supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            if (tracker.selection.size() > 0) {
+                title = getString(R.string.selected, tracker.selection.size())
+                inflateMenu(R.menu.profiles_selected_menu)
+                menu.findItem(R.id.action_edit).isVisible = tracker.selection.size() == 1
+                supportActionBar?.setHomeAsUpIndicator(R.drawable.close_24)
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                setOnMenuItemClickListener { handleMenuItemClick(it) }
+            } else {
+                title = getString(R.string.app_name)
+                inflateMenu(R.menu.main_menu)
+                setOnMenuItemClickListener { onOptionsItemSelected(it) }
+            }
+        }
+    }
+
+    private fun handleMenuItemClick(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.action_edit -> {
+                val intent = SoundProfileManager.createIntent(this, tracker.selection.first().toInt())
+                soundProfileManagerLauncher.launch(intent)
+                tracker.clearSelection()
+                true
+            }
+            R.id.action_delete -> {
+                mainViewModel.deleteSoundProfiles(
+                    (binding.recyclerView.adapter as SoundProfileRecyclerAdapter).getSelectedSoundProfile()
+                )
+                tracker.clearSelection()
+                true
+            }
+            R.id.action_delete_all -> {
+                mainViewModel.deleteAllSoundProfiles()
+                tracker.clearSelection()
+                true
+            }
+            R.id.action_select_all -> {
+                (binding.recyclerView.adapter as SoundProfileRecyclerAdapter).selectAll()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                tracker.clearSelection()
+                true
+            }
+            R.id.infoMenu -> showInfoMenu()
+            R.id.shareMenu -> {
+                shareApp(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showInfoMenu(): Boolean {
+        AboutDialog().show(supportFragmentManager, "about_dialog_box")
+        return true
+    }
 }
 
 fun getCurrentVolume(context: Context): SoundProfile {
-  val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
-  val minMediaVolume = audioManager.getStreamMinVolume(AudioManager.STREAM_MUSIC)
-  val maxMediaVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
-  val minRingerVolume = audioManager.getStreamMinVolume(AudioManager.STREAM_RING)
-  val maxRingerVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING)
-  val minCallVolume = audioManager.getStreamMinVolume(AudioManager.STREAM_VOICE_CALL)
-  val maxCallVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL)
-  val minAlarmVolume = audioManager.getStreamMinVolume(AudioManager.STREAM_ALARM)
-  val maxAlarmVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM)
-  val minNotificationVolume = audioManager.getStreamMinVolume(AudioManager.STREAM_NOTIFICATION)
-  val maxNotificationVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION)
+    fun getVolume(stream: Int): Float {
+        val min = audioManager.getStreamMinVolume(stream)
+        val max = audioManager.getStreamMaxVolume(stream)
+        return (audioManager.getStreamVolume(stream) - min) / (max - min).toFloat()
+    }
 
-  val currentMediaVolume =
-      (audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) - minMediaVolume) /
-          (maxMediaVolume - minMediaVolume).toFloat()
-  val currentRingerVolume =
-      (audioManager.getStreamVolume(AudioManager.STREAM_RING) - minRingerVolume) /
-          (maxRingerVolume - minRingerVolume).toFloat()
-  val currentCallVolume =
-      (audioManager.getStreamVolume(AudioManager.STREAM_VOICE_CALL) - minCallVolume) /
-          (maxCallVolume - minCallVolume).toFloat()
-  val currentAlarmVolume =
-      (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) - minAlarmVolume) /
-          (maxAlarmVolume - minAlarmVolume).toFloat()
-  val currentNotificationVolume =
-      (audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION) - minNotificationVolume) /
-          (maxNotificationVolume - minNotificationVolume).toFloat()
-
-  return SoundProfile(
-      id = 0,
-      title = "Current Profile ${Date()}",
-      description = "Current volume settings",
-      mediaVolume = currentMediaVolume,
-      notificationVolume = currentNotificationVolume,
-      ringerVolume = currentRingerVolume,
-      callVolume = currentCallVolume,
-      alarmVolume = currentAlarmVolume,
-      startTime = Date(),
-      endTime = Date(),
-      isActive = false,
-      repeatEveryday = false,
-      repeatDays = emptyList())
+    return SoundProfile(
+        id = 0,
+        title = "Current Profile ${Date()}",
+        description = "Current volume settings",
+        mediaVolume = getVolume(AudioManager.STREAM_MUSIC),
+        notificationVolume = getVolume(AudioManager.STREAM_NOTIFICATION),
+        ringerVolume = getVolume(AudioManager.STREAM_RING),
+        callVolume = getVolume(AudioManager.STREAM_VOICE_CALL),
+        alarmVolume = getVolume(AudioManager.STREAM_ALARM),
+        startTime = Date(),
+        endTime = Date(),
+        isActive = false,
+        repeatEveryday = false,
+        repeatDays = emptyList()
+    )
 }
